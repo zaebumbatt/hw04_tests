c475a4350b5524e9507e0257c9c000eda04a7d48
Nikita Ustiuzhanin <zaebumba@gmail.com>
2020-08-05 12:45:44 +0700

    Fixed mistakes

--- posts/tests.py
+++ posts/tests.py
@@ -1,38 +1,56 @@
 from django.test import TestCase, Client
 
-from posts.models import User, Post
+from posts.models import User, Post, Group
 
 
-class TestStringMethods(TestCase):
+class TestPosts(TestCase):
     def setUp(self):
-        self.client = Client()
+        self.login_client = Client()
+        self.logout_client = Client()
         self.user = User.objects.create_user(username="test", password="testpassword12345", email="test@yandex.ru")
-        self.client.login(username='test', password='testpassword12345')
-        self.post = Post.objects.create(text='Test post', author=self.user)  # Check if authorized user can create a post
-
-    def test_profile(self):
-        response = self.client.get(f"/{self.user}/")
-        self.assertEqual(response.status_code, 200)  # Check if new user profile page exists
+        self.login_client.force_login(self.user)
+        self.logout_client.logout()
+        self.group = Group.objects.create(title='testgroup', slug='testgroup')
+        self.post = Post.objects.create(text='Test post', author=self.user, group=self.group)  # Check if authorized user can create a post
 
     def test_new_post(self):
-        response_index = self.client.get("")
+        response_index = self.login_client.get("")
         self.assertIn(self.post.text, str(response_index.content))  # Check if new post added to /index/ page
 
-        response_profile = self.client.get(f"/{self.user}/")
+        response_profile = self.login_client.get(f"/{self.user}/")
         self.assertIn(self.post.text, str(response_profile.content))  # Check if new post added to /profile/page
 
-        response_edit = self.client.get(f"/{self.user}/{self.post.id}/")
+        response_edit = self.login_client.get(f"/{self.user}/{self.post.id}/")
         self.assertIn(self.post.text, str(response_edit.content))  # Check if new post added to /profile/post_id page
 
     def test_edit_post(self):
         self.post = Post.objects.get(id=self.post.id)
         self.post.text = 'Test post after update'
         self.post.save()
-        response_edit = self.client.get(f"/{self.user}/{self.post.id}/")
-        self.assertIn('Test post after update', str(response_edit.content))  # Check if post was edited from authorized user
+
+        response_index = self.login_client.get("")
+        self.assertIn('Test post after update', str(response_index.content))  # Check if post was edited on /index/ page
+
+        response_profile = self.login_client.get(f"/{self.user}/")
+        self.assertIn('Test post after update', str(response_profile.content))  # Check if post was edited on /profile/ page
+
+        response_edit = self.login_client.get(f"/{self.user}/{self.post.id}/")
+        self.assertIn('Test post after update', str(response_edit.content))  # Check if post was edited on /profile/post_id page
+
+        response_group = self.login_client.get(f"/group/{self.group}/")
+        self.assertIn('Test post after update', str(response_group.content))  # Check if post was edited on /group/ page_number
 
     def test_unauthorized(self):
-        self.client.logout()
-        response = self.client.get("/new/")
+        response = self.logout_client.post('/new/', {'text': 'Test post', 'author': 'anonymous'})
         self.assertEqual(response['location'], '/auth/login/?next=/new/')  # Check if redirect goes to login page
         self.assertEqual(response.status_code, 302)  # Check if unauthorized user redirects when trying to use /new/ page
+
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="test", password="testpassword12345", email="test@yandex.ru")
+
+    def test_profile(self):
+        response = self.client.get(f"/{self.user}/")
+        self.assertEqual(response.status_code, 200)  # Check if new user profile page exists
--- posts/views.py
+++ posts/views.py
@@ -37,12 +37,10 @@ def group_posts(request, slug):
 @login_required
 def new_post(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST or None)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.text = form.cleaned_data['text']
-            post.group = form.cleaned_data['group']
             post.save()
             return redirect('index')
     else:
@@ -51,10 +49,10 @@ def new_post(request):
 
 
 def profile(request, username):
-    user = User.objects.get(username=username)
+    user = get_object_or_404(User, username=username)
     fullname = user.get_full_name()
-    posts_count = Post.objects.filter(author=user).count()
-    post_list = Post.objects.filter(author=user)
+    posts_count = user.posts.filter(author=user).count()
+    post_list = user.posts.filter(author=user)
     paginator = Paginator(post_list, 1)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
@@ -69,10 +67,10 @@ def profile(request, username):
 
 
 def post_view(request, username, post_id):
-    user = User.objects.get(username=username)
+    user = get_object_or_404(User, username=username)
     fullname = user.get_full_name()
-    post = Post.objects.get(id=post_id)
-    posts_count = Post.objects.filter(author=user).count()
+    post = get_object_or_404(Post, id=post_id)
+    posts_count = user.posts.filter(author=user).count()
     context = {
         'username': user,
         'fullname': fullname,
@@ -84,18 +82,16 @@ def post_view(request, username, post_id):
 
 @login_required
 def post_edit(request, username, post_id):
-    user = User.objects.get(username=username)
-    post = Post.objects.get(id=post_id)
+    user = get_object_or_404(User, username=username)
+    post = get_object_or_404(Post, id=post_id)
     if post.author == request.user:
         if request.method == "POST":
-            form = PostForm(request.POST)
+            form = PostForm(request.POST or None)
             if form.is_valid():
                 post = form.save(commit=False)
                 post.author = user
                 post.id = post_id
                 post.pub_date = datetime.datetime.now()
-                post.text = form.cleaned_data['text']
-                post.group = form.cleaned_data['group']
                 post.save()
                 return redirect('post', username=user, post_id=post_id)
         else:
